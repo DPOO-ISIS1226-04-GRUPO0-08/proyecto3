c6a58dd2c211341171174e103b78f8c37c8bff3e
Dreyc10 <111070788+Dreyc10@users.noreply.github.com>
2023-06-04 23:20:28 -0500

    pruebas

--- src/tests/TestInventaro.java
+++ src/tests/TestInventaro.java
@@ -5,20 +5,56 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import Controller.Inventario;
 
 class TestInventaro {
-
+	
+	private String habitacion ;
+	private String platos ;
+	private int idreserva;
+	private String nombre;
+	private Inventario inventario;
+	
 	@BeforeEach
-	void setUp() throws Exception {
+	private void setUp() throws Exception {
+		habitacion = "//datos//habitaciones.csv";
+		platos = "//datos//platos.csv";
+		inventario = new Inventario() ;
+		nombre = "prueba1";
+		idreserva = -1;
+		
 	}
+	
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testCargarHabitaciones()throws Exception {
+		setUp();
+		try {
+			inventario.cargarHabitaciones(habitacion);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	 
+		
+		
 	}
+	@Test
+	public void testCargarPlatos()throws Exception {
+		setUp();
+		setUp();
+		try {
+			inventario.cargarHabitaciones(platos);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void testCalcularTotal() throws Exception {
+		setUp();
+		assertEquals(0.0, inventario.calcularTotal(nombre));
+	}
+	
+	
 
 }
